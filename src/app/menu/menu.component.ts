7ef8f19e4d46e9bcba8a44b3d94c0ecc5813d1b8
Jeferson Knop <jefersonknop@gmail.com>
2020-05-20 20:40:23 -0300

    primeiro commit

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  items: MenuItem[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.items = [
+        {
+            label: 'Home', icon: 'pi pi-fw pi-times',  routerLink: ['/']
+            
+        },
+        {
+            label: 'Consultas',
+            icon: 'pi pi-fw pi-file',
+            items: [
+               
+                {
+                    label: 'Estados', 
+                    icon: 'pi pi-fw pi-plus',
+                    items: [
+                       
+                        {label: 'Estados primeng',  routerLink: ['/estados_primeng']},   
+                        {label: 'Estados css',  routerLink: ['/estados']},   
+                    ]
+                },
+                {
+                    label: 'Cidades', 
+                    icon: 'pi pi-fw pi-plus',
+                    items: [
+                       
+                        {label: 'Cidades primeng'},   
+                        {label: 'Cidade css', routerLink: ['/cidades']},
+                    ]
+                }
+               
+            ]
+        }
+    ];
+}
+}
